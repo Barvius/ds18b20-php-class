8630b118acd9487a9483f20940ab5f2dd4e53a2e
barvius <barabanshikov.oleg@yandex.ru>
2017-06-25 00:49:56 +0300

    update readme

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # ds18b20-php-class
 php class from reading ds18b20 sensors from RPI/BPI/OPI
+``` php
+$ds = new DS18B20;
+
+for ($i=0; $i < count($ds->Ids); $i++) {
+  echo $ds->GetData($ds->Ids[$i])." || ";
+  echo $ds->Ids[$i];
+}
+```
---
+++ ds18b20.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * ds18b20
+ */
+class DS18B20 {
+
+  var $Ids;
+
+  function __construct(){
+    $this->Ids=file('/sys/devices/w1_bus_master1/w1_master_slaves');
+  }
+
+  public function GetData($id=''){
+    $id = preg_split("/\n/", $id)[0];
+    $patch = "/sys/bus/w1/devices/$id/w1_slave";
+    $f = fopen($patch, "r");
+    $data = fread($f, filesize($patch));
+    preg_match("/YES/", $data, $crc);
+    preg_match("/t=(.+)/", preg_split("/\n/", $data)[1], $val);
+    return number_format($val[1] / 1000, 3, '.', '');
+  }
+
+}
+
+?>
